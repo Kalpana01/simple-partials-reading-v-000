b2b30aa40cc71d1e72fb643a4a415de9d2ad3c11
Kalpana Poosarla <joyci09@gmail.com>
2018-07-23 21:27:09 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,23 +13,23 @@ class PostsController < ApplicationController
   end
 
   def create
-    @author = Author.first
-    @post = Post.new
-    @post.title = params[:title]
-    @post.description = params[:description]
-    @post.author_id = @author.id
-    @post.save
-    redirect_to post_path(@post)
+       @author = Author.first
+       @post = Post.new
+       @post.title = params[:title]
+       @post.description = params[:description]
+       @post.author_id = @author.id
+       @post.save
+       redirect_to post_path(@post)
+     end
+   
+     def edit
+       @post = Post.find(params[:id])
+     end
+   
+     def update
+       @post = Post.find(params[:id])
+       @post.update(title: params[:title], description: params[:description])
+       redirect_to post_path(@post)
+     end
+    
   end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.update(title: params[:title], description: params[:description])
-    redirect_to post_path(@post)
-  end
-  
-end
